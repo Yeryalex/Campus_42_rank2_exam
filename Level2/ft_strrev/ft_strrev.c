4380030d09bf739a05d7f74b5338eeb13e26683f
Yery <yeryalex03@gmail.com>
2024-03-28 12:00:15 +0100

    ft_strrev done

--- Level2/ft_strrev/ft_strrev.c
+++ Level2/ft_strrev/ft_strrev.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-
 int ft_strlen(char *str)
 {
     int i;
@@ -14,34 +11,17 @@ int ft_strlen(char *str)
 char *ft_strrev(char *str)
 {
     int i;
-    int j;
+    int len;
     char c;
 
     i = 0;
-    j = ft_strlen(str) - 1;
-    while (str[i] && i <= j / 2)
+    len = ft_strlen(str);
+    while (str[i] && i < len / 2)
     {
         c = str[i];
-        str[i] = str[j];
-        str[j] = c;
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = c;
         i++;
-        j--;
     }
-    str[i] = '\0';
     return (str);
-}
-
-int main()
-{
-    char str[] = "hello, world";
-    char str2[] = "hello, world";
-    char *reversed;
-    char *reversed2;
-
-    reversed = strrev(str);
-    reversed2 = ft_strrev(str2);
-
-    printf("%s\n", reversed);
-    printf("%s\n", reversed2);
-    return (0);
 }
